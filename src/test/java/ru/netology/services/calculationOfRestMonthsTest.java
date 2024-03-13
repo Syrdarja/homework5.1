5483629e2edb9264e2f522bf329e7a98161e1e66
MariMax <maxim.prohachev@yandex.ru>
2024-03-13 22:16:33 +0300

    csv commit

--- src/test/java/ru/netology/services/calculationOfRestMonthsTest.java
+++ src/test/java/ru/netology/services/calculationOfRestMonthsTest.java
@@ -1,27 +1,15 @@
 package ru.netology.services;
-import org.testng.annotations.Test;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 public class calculationOfRestMonthsTest {
-    @Test
-    void calculationOfRestMonthsIncome10_000 () {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/rest.csv")
+    void calculationOfRestMonths(int income, int expenses, int threshold, int expected) {
         calculationOfRestMonths service = new calculationOfRestMonths();
-        int income = 10_000;
-        int expenses = 3000;
-        int threshold = 20_000;
-        int expected = 3;
-
-        int actual = service.calculate (income, expenses, threshold);
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
-    @Test
-    void calculationOfRestMonthsIncome100_000 () {
-        calculationOfRestMonths service = new calculationOfRestMonths();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
 
-        int actual = service.calculate (income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
-    }
 }
