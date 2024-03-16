ce6db692bdf88e400f36c2447e8e040ecc890082
MariMax <maxim.prohachev@yandex.ru>
2024-03-16 16:54:23 +0300

    correct commit

--- src/test/java/ru/netology/services/СalculationOfRestMonthsTest.java
+++ src/test/java/ru/netology/services/СalculationOfRestMonthsTest.java
@@ -3,11 +3,12 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-public class calculationOfRestMonthsTest {
+
+public class СalculationOfRestMonthsTest {
     @ParameterizedTest
-    @CsvFileSource(files="src/test/resources/rest.csv")
-    void calculationOfRestMonths(int income, int expenses, int threshold, int expected) {
-        calculationOfRestMonths service = new calculationOfRestMonths();
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    void СalculationOfRestMonths(int income, int expenses, int threshold, int expected) {
+        СalculationOfRestMonths service = new СalculationOfRestMonths();
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
